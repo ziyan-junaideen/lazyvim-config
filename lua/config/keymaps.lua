c0b074d3a5ad829fadd43dd7fd3edd1038c6deed
Ziyan Junaideen <ziyan@jdeen.com>
2023-08-09 20:40:01 +0530

    Tmux NVIM navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("v", "<leader>y", '"+y', { silent = true, noremap = true })
+vim.keymap.set("v", "<leader>p", '"+p', { silent = true, noremap = true })
+vim.keymap.set("v", "<leader>Y", '"*p', { silent = true, noremap = true })
+vim.keymap.set("v", "<leader>P", '"*p', { silent = true, noremap = true })
