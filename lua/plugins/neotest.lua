0450166c63e96846a8104f064041c60031b52db8
Ziyan Junaideen <ziyan@tryedge.com>
2023-08-09 19:45:23 +0530

    Auto quote

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,26 @@
+return {
+  { "tpope/vim-vinegar", keys = { { "-", desc = "Explore folder" } } },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "olimorris/neotest-rspec",
+    },
+    opts = {
+      adapters = {
+        ["neotest-rspec"] = {
+          rspec_cmd = function()
+            return vim.tbl_flatten({
+              "bundle",
+              "exec",
+              "rspec",
+            })
+          end,
+        },
+      },
+    },
+  },
+}
