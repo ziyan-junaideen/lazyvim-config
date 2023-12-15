1a754a652ae042172ff7081e7c915d0fbdb0ce5e
Ziyan Junaideen <ziyan@jdeen.com>
2023-12-15 14:37:14 +0530

    Update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,11 +6,55 @@ return {
         solargraph = {
           mason = false,
         },
+        rubocop = {
+          mason = false,
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local util = require("lspconfig.util")
+
+          opts.filetypes = { "html", "elixir", "eelixir", "heex" }
+
+          opts.init_options = {
+            userLanguages = {
+              elixir = "html-eex",
+              eelixir = "html-eex",
+              heex = "html-eex",
+            },
+          }
+          opts.settings = {
+            tailwindCSS = {
+              experimental = {
+                classRegex = {
+                  'class[:]\\s*"([^"]*)"',
+                },
+              },
+            },
+          }
+
+          opts.root_dir = function(fname)
+            return util.root_pattern(
+              "tailwind.config.js",
+              "tailwind.config.cjs",
+              "tailwind.config.mjs",
+              "tailwind.config.ts",
+              "postcss.config.js",
+              "postcss.config.cjs",
+              "postcss.config.mjs",
+              "postcss.config.ts",
+              "mix.exs",
+              "Gemfile"
+            )(fname) or util.find_package_json_ancestor(fname) or util.find_node_modules_ancestor(fname) or util.find_git_ancestor(
+              fname
+            )
+          end
+        end,
       },
     },
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
 
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "rebelot/terminal.nvim",
+    keys = {
+      {
+        "\\t",
+        "<cmd>TermToggle<cr>",
+        desc = "Toggle terminal panel",
+      },
+    },
+    config = true,
+  },
+}
