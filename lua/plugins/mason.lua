fb64903ee279e6e5a88524e184e4a53b69d24233
Ziyan Junaideen <ziyan@jdeen.com>
2024-03-08 15:42:00 +0530

    Update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -54,11 +54,24 @@ return {
     },
   },
   {
-    "nvimtools/none-ls.nvim",
+    "stevearc/conform.nvim",
     opts = function(_, opts)
-      local nls = require("null-ls")
-
-      table.insert(opts.sources, nls.builtins.formatting.erb_format)
+      opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
+        eruby = { "erb_format" },
+      })
+      opts.formatters = {
+        erb_format = {
+          args = { "--single-class-per-line", "--stdin" },
+        },
+      }
     end,
   },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+
+  --     -- table.insert(opts.sources, nls.builtins.formatting.erb_format)
+  --   end,
+  -- },
 }
